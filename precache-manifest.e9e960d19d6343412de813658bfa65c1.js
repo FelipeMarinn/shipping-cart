7aa332ee90922fa036e8d942667e0174003862e7
felipe <felipeguisao18@gmail.com>
2022-11-19 16:59:46 -0500

    Updates

---
+++ precache-manifest.e9e960d19d6343412de813658bfa65c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e705a2ebbb30b6f3b69",
+    "url": "/shipping-cart/static/css/main.429d52a2.chunk.css"
+  },
+  {
+    "revision": "9e705a2ebbb30b6f3b69",
+    "url": "/shipping-cart/static/js/main.7879d96a.chunk.js"
+  },
+  {
+    "revision": "f46abf6e2340c1f63bea",
+    "url": "/shipping-cart/static/js/runtime~main.28dffefc.js"
+  },
+  {
+    "revision": "2e53f5093ff2e546ba32",
+    "url": "/shipping-cart/static/js/2.a01a0184.chunk.js"
+  },
+  {
+    "revision": "543ad8e6696915cffe3b",
+    "url": "/shipping-cart/static/js/3.27ddafa2.chunk.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/shipping-cart/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/shipping-cart/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/shipping-cart/static/media/times-solid.2c5552a1.svg"
+  },
+  {
+    "revision": "cc7126a3f24e077972865ef8cc9f678b",
+    "url": "/shipping-cart/index.html"
+  }
+];
