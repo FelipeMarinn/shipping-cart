9a389623e8990453c566c642da610209c8588e06
felipe <felipeguisao18@gmail.com>
2022-11-22 22:08:57 -0500

    error handling in stripe and css fix

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,28 +1,78 @@
 import React, {Component} from 'react';
 import Products from './section/Products'
 import Details from './section/Details'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Cart from './section/Cart'
+import Cancel from './section/Cancel';
+import Success from './section/Success';
 import { About } from '../pages/About';
 import { Contact } from '../pages/Contact';
-import { Cancel } from './section/Cancel';
-import { Success } from './section/Success';
 
+
+const routes = [
+  {
+    path: "/product/:id",
+    component: Details
+  },
+  {
+    path: "/cart",
+    component: Cart
+  },
+  {
+    path: "/success",
+    component: Success,
+  },
+  {
+    path: "/cancel",
+    component: Cancel,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+  {
+    path: "/contact",
+    component: Contact,
+  },
+  {
+    path: "/",
+    component: Products,
+  },
+];
 
 export default class Section extends Component{
   render() {
     return(
       <div>
         <section>
-          <Route path="/" component={Products} exact/>
-          <Route path="/product/:id" component={Details} />
-          <Route path="/cart" component={Cart} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/success" component={Success} />
-          <Route path="/cancel" component={Cancel} />
+          <Switch>
+            {/* <Route path="/product/:id" component={Details} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/success" component={Success} />
+            <Route path="/cancel" component={Cancel} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/" component={Products} /> */}
+
+            {routes.map((route, i) => (
+              <RouteWithSubRoutes key={i} {...route} />
+            ))}
+
+          </Switch>
         </section>
       </div>
     )
   }
+}
+
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
 }
--- src/components/section/Success.js
+++ src/components/section/Success.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react';
+import '../css/Payment.css'
 
-export const Success = () => {
-  return (
-    <div className='payment-result'>
-     <div className='payment__statement'>
-       <h1 className='statement'>Success</h1>
-       <h2 className='statement'>Thank you for your purchase!</h2>
-     </div>   
-    </div>
-  )
+
+export class Success extends Component {
+  render() {
+    return (
+      <div className='payment-result'>
+        <div className='payment__statement'>
+          <h1 className='statement'>Cancel</h1>
+          <h2 className='statement'>Your payment was canceled</h2>
+        </div>   
+      </div>
+    )
+  }  
 }
+
+export default Success
